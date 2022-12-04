e4adff3d32199cbb6ca3f35bd8b9c28129ddd0c8
progressiveOverload <108024533+progressiveOverload@users.noreply.github.com>
2022-12-04 14:31:40 +0300

    first commit

--- lib/calisan_kopya.dart
+++ lib/calisan_kopya.dart
@@ -1,23 +1,20 @@
+// ignore_for_file: unused_field
+
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:searchfield/searchfield.dart';
 import 'ingredient_model.dart';
 
-//# local database: we need hide and hive flutter. add libraries in pubspec.yaml file
-// hive: ^2.2.1
-//  hive_flutter: ^1.1.0
-
 void main() async {
-  //initialize hive db
   await Hive.initFlutter();
-  //open the shopping box
-  await Hive.openBox("shopping_box");
+
+  await Hive.openBox("ingredient_box");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,23 +36,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //define a variable to shopping box reference
-  final _shoppingBox = "shopping_box";
+  final _ingredientBox = "ingredient_box";
 
-  //form key for validating the form fields
   final _formKey = GlobalKey<FormState>();
-  // Create a text controller and use it to retrieve the current value of the TextField.
+
   final _itemNameController = TextEditingController();
 
-  // final _countController = TextEditingController();
   final focus = FocusNode();
 //dispose as necessarily
   @override
   void dispose() {
     focus.dispose();
-    Hive.close(); //closes all open boxes
+    Hive.close();
     _itemNameController.dispose();
-    // _countController.dispose(); //dispose controllers
+
     super.dispose();
   }
 
@@ -68,12 +62,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   bool containsIngredients(String text) {
-    final Ingredient? result = ingredients.firstWhere(
+    final Ingredient result = ingredients.firstWhere(
         (Ingredient country) =>
             country.name.toLowerCase() == text.toLowerCase(),
         orElse: () => Ingredient.init());
 
-    if (result!.name.isEmpty) {
+    if (result.name.isEmpty) {
       return false;
     }
     return true;
@@ -92,28 +86,16 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         color: const Color.fromARGB(255, 27, 198, 210),
-        //screen decoration
-        //alignment: Alignment.center,
         width: double.infinity,
         padding: const EdgeInsets.only(bottom: 44),
-        // decoration: const BoxDecoration(
-        //     gradient: LinearGradient(
-        //         begin: Alignment.topLeft,
-        //         end: Alignment.bottomRight,
-        //         colors: [
-        //       Color(0XFF1f0f47),
-        //       Color(0XFF1e7d81),
-        //     ])),
-        //use a listenable builder, it will make sure it builds whenever needed
         child: ValueListenableBuilder(
-          valueListenable: Hive.box(_shoppingBox).listenable(),
+          valueListenable: Hive.box(_ingredientBox).listenable(),
           builder: (BuildContext context, Box box, _) {
             //use keys to get item names
             if (box.keys.isEmpty) {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  //cartHeadWidget("0"),
                   Container(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 30, vertical: 20),
@@ -130,13 +112,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               );
             }
-            //list to hold items to show in listview
+
             List itemNames = [];
-            //add header item at index 0,
+
             itemNames.add("card head");
-            //add items   and reverse items names to newest first
+
             itemNames.addAll(box.keys.toList().reversed.toList());
-            //list view builder
+
             return ListView.builder(
                 itemCount: itemNames.length,
                 itemBuilder: (context, index) {
@@ -145,10 +127,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         .toString()); //remove the head item
                   }
                   return shoppingRowItem(
-                      itemNames.elementAt(index).toString(),
-                      Hive.box(_shoppingBox)
-                          .get(itemNames.elementAt(index))
-                          .toString());
+                    itemNames.elementAt(index).toString(),
+                    Hive.box(_ingredientBox)
+                        .get(itemNames.elementAt(index))
+                        .toString(),
+                  );
                 });
           },
         ),
@@ -156,12 +139,10 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
           backgroundColor: const Color.fromARGB(255, 31, 12, 82),
           onPressed: () {
-            //launch add shopping item dialog
             addNewShoppingItem(context);
           },
           tooltip: 'Malzeme ekle',
-          child: const Icon(Icons
-              .add)), // This trailing comma makes auto-formatting nicer for build methods.
+          child: const Icon(Icons.add)),
     );
   }
 
@@ -170,7 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return FractionallySizedBox(
       widthFactor: 0.21,
       child: Container(
-        // padding: const EdgeInsets.only(left: 0),
+        padding: const EdgeInsets.only(left: 0),
         margin: const EdgeInsets.only(top: 25, bottom: 15),
         decoration: const BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(5)),
@@ -192,7 +173,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  //single shopping item
   Widget shoppingRowItem(String name, String count) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
@@ -206,7 +186,6 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          //leading icon the cart item
           Flexible(
             child: Container(
               alignment: Alignment.center,
@@ -281,119 +260,23 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  //edit shopping item
-  // void editShoppingItem(BuildContext context, String item, String count) {
-  //   showDialog(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         //set initial values before it
-  //         _itemNameController.text = item;
-  //         _countController.text = count;
-  //         return AlertDialog(
-  //           title: const Text("Edit & Save Item"),
-  //           content: Form(
-  //               key: _formKey,
-  //               child: Column(
-  //                 mainAxisSize:
-  //                     MainAxisSize.min, //the dialog takes only size it needs
-  //                 children: [
-  //                   Padding(
-  //                     padding: const EdgeInsets.only(top: 8, bottom: 4),
-  //                     child: TextFormField(
-  //                       controller:
-  //                           _itemNameController, //will help get the field value on submit
-  //                       decoration: const InputDecoration(
-  //                         border: OutlineInputBorder(),
-  //                         labelText: 'Item Name',
-  //                       ),
-  //                       //validator on submit, must return null when every thing ok
-  //                       // The validator receives the text that the user has entered.
-  //                       validator: (value) {
-  //                         if (value == null || value.isEmpty) {
-  //                           return 'Empties not allowed';
-  //                         } else if (value.trim().isEmpty) {
-  //                           return "Empties not allowed";
-  //                         }
-  //                         return null;
-  //                       },
-  //                     ),
-  //                   ),
-  //                   Padding(
-  //                     padding: const EdgeInsets.only(top: 8, bottom: 4),
-  //                     child: TextFormField(
-  //                       controller:
-  //                           _countController, //will help get the field value on submit
-  //                       // initialValue: "1",
-  //                       keyboardType: TextInputType.number,
-  //                       decoration: const InputDecoration(
-  //                         border: OutlineInputBorder(),
-  //                         labelText: 'Count',
-  //                       ),
-  //                       //validator on submit, must return null when every thing ok
-  //                       // The validator receives the text that the user has entered.
-  //                       validator: (value) {
-  //                         if (value == null || value.isEmpty) {
-  //                           return 'Empties not allowed';
-  //                         } else if (value.trim().isEmpty) {
-  //                           return "Empties not allowed";
-  //                         }
-  //                         return null;
-  //                       },
-  //                     ),
-  //                   ),
-  //                 ],
-  //               )),
-  //           //actions
-  //           actions: [
-  //             //dismiss dialog
-  //             TextButton(
-  //                 onPressed: () {
-  //                   Navigator.of(context).pop(); // dismiss dialog
-  //                 },
-  //                 child: const Text("Cancel")),
-
-  //             //save btn
-  //             TextButton(
-  //               child: const Text("Save"),
-  //               onPressed: () {
-  //                 // Validate returns true if the form is valid, or false otherwise.
-  //                 if (_formKey.currentState!.validate()) {
-  //                   //name used as a key and count is used as a value.
-  //                   //delete the prev and add new
-  //                   Hive.box(_shoppingBox).delete(item);
-  //                   //use put to add a key-value map
-  //                   Hive.box(_shoppingBox).put(_itemNameController.text,
-  //                       _countController.text); //key is the item name.
-  //                   _itemNameController.clear(); //clear text in field
-  //                   _countController.clear(); //clear the field
-  //                   Navigator.of(context).pop(); // dismiss dialog
-  //                 }
-  //               },
-  //             ),
-  //           ],
-  //         );
-  //       });
-  // }
-
-  //delete a shopping item from the shopping box
   void deleteShoppingItem(BuildContext context, String item) {
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text(" Dolabımdan \"$item\" çıkar "),
-            // content: Text("Confirm to remove \"$item\" from the shopping list"),
+            title: Text(" Dolabımdan \"$item\" çıkar? "),
             actions: [
               TextButton(
                   onPressed: () {
-                    Navigator.of(context).pop(); // dismiss dialog
+                    Navigator.of(context).pop();
                   },
                   child: const Text("İptal")),
               TextButton(
                 child: const Text("Çıkar"),
                 onPressed: () {
-                  Hive.box(_shoppingBox).delete(item); //key is item name.
-                  Navigator.of(context).pop(); // dismiss dialog
+                  Hive.box(_ingredientBox).delete(item);
+                  Navigator.of(context).pop();
                 },
               ),
             ],
@@ -401,7 +284,6 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
-  //add new shopping item to the box dialog
   void addNewShoppingItem(BuildContext context) {
     showDialog(
         context: context,
@@ -411,8 +293,7 @@ class _MyHomePageState extends State<MyHomePage> {
             content: Form(
                 key: _formKey,
                 child: Column(
-                  mainAxisSize:
-                      MainAxisSize.min, //the dialog takes only size it needs
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 8, bottom: 4),
@@ -425,18 +306,9 @@ class _MyHomePageState extends State<MyHomePage> {
                                 item: malzeme))
                             .toList(),
                         controller: _itemNameController,
-                        //VALİDATORLA OYNAMADAN ÖNCEKİ
-                        /* validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Burası boş olmamalı 🤔';
-                          } else if (value.trim().isEmpty) {
-                            return 'Burası boş olmamalı 🤔';
-                          } else if (!containsIngredients(value)) {}
-                          return null;
-                          */
                         validator: (value) {
                           if (value!.isEmpty || !containsIngredients(value)) {
-                            return 'Please Enter a valid ingredient';
+                            return 'Böyle bir malzeme bulamadık 🤔 ';
                           }
                           return null;
                         },
@@ -456,24 +328,21 @@ class _MyHomePageState extends State<MyHomePage> {
               TextButton(
                 child: const Text("İptal"),
                 onPressed: () {
-                  //    showToast(context, "Canceled");  //toast a cancelled sms
-                  Navigator.of(context).pop(); // dismiss dialog
+                  showToast(context, "İptal Edildi");
+                  Navigator.of(context).pop();
                 },
               ),
               TextButton(
                 child: const Text("Ekle"),
                 onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
                   if (_formKey.currentState!.validate()) {
-                    //name used as a key and count is used as a value.
-                    //use put to add a key-value map
-                    Hive.box(_shoppingBox).put(
-                        _itemNameController.text, 1); //key is the item name.
-                    //show saved values
-                    //   showToast(context, "Saved! Item: "+_itemNameController.text +", Count: "+_countController.text);
-                    _itemNameController.clear(); //clear text in field
-                    // _countController.clear(); //clear the field
-                    Navigator.of(context).pop(); // dismiss dialog
+                    Hive.box(_ingredientBox).put(_itemNameController.text, 1);
+
+                    showToast(
+                        context, "${_itemNameController.text} kaydedildi!");
+                    _itemNameController.clear();
+
+                    Navigator.of(context).pop();
                   }
                 },
               ),
@@ -482,16 +351,16 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
-  //define a toast method
-  // void showToast(BuildContext context, String text) {
-  //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-  //     content: Text(
-  //       text,
-  //       textAlign: TextAlign.center,
-  //       style: const TextStyle(color: Colors.deepOrange, fontSize: 14),
-  //     ),
-  //     behavior: SnackBarBehavior.floating,
-  //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
-  //   ));
-  // }
+  void showToast(BuildContext context, String text) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(seconds: 1),
+      content: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: const TextStyle(color: Colors.white, fontSize: 14),
+      ),
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+    ));
+  }
 }
--- lib/ingredient_model.dart
+++ lib/ingredient_model.dart
@@ -14,5 +14,50 @@ class Ingredient {
 }
 
 List<Map<String, Object>> data = [
-  {'ingredient_name': 'Afghanistan', 'category_name': "Baharat"}
+  {
+    'ingredient_name': 'Nohut',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Fasulye',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Kırmızı Mercimek',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Bakla',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Bezelye',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Börülce',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Barbunya',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Soya Fasulyesi',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Yeşil Mercimek',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Sarı Mercimek',
+    'category_name': "Baklagil",
+  },
+  {
+    'ingredient_name': 'Mısır',
+    'category_name': "Baklagil",
+  },
+//baklagil_son
+  //balık_başlangıç
 ];
